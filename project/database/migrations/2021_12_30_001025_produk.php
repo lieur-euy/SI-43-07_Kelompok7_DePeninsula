1ced674b2367f73a8a4e7d370a1ecb2aba9f431d
yayathidayatulloh <57533243+yayathidayatulloh@users.noreply.github.com>
2022-01-03 18:18:49 +0700

    tubes 1

---
+++ project/database/migrations/2021_12_30_001025_produk.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Produk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('harga');
+            $table->string('stock');
+            $table->string('img');
+            $table->string('kategori');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ project/database/migrations/2021_12_30_002051_pembelian.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Pembelian extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembelian', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_order_produk');
+            $table->string('id_pembelian');
+            $table->string('id_produk');
+            $table->string('jumlah');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
